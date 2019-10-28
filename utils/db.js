1fc0c8a36e40c9ef2c0112ed9cced4a9b1f1638c
Jalil1103 <abdijalildini@protonmail.com>
2019-10-28 14:10:13 -0400

    initial commit

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,6 @@ mongoose.connect(
 );
 
 const db = mongoose.connection;
-
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("Connected to the DB!");
