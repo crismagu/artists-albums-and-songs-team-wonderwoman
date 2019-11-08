392c39a79f0b05b444f92d419599e9987c867844
crismagu <cmg1205@gmail.com>
2019-11-08 15:05:59 -0500

    fixed cors error

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const db = require('./src/utils/db');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const db = require("./src/utils/db");
 
 // var indexRouter = require('./src/routes/index-router');
-const artistRouter = require('./src/routes/artist-router');
-const albumRouter = require('./src/routes/album-router');
+const artistRouter = require("./src/routes/artist-router");
+const albumRouter = require("./src/routes/album-router");
 
 var app = express();
+app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.use('/', indexRouter);
-app.use('/artists', artistRouter);
-app.use('/albums', albumRouter);
+app.use("/artists", artistRouter);
+app.use("/albums", albumRouter);
 
 module.exports = app;
